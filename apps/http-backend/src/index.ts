7cd57644b504bcf552cc4a88f9fd794b7875d69f
vikas jogi <jogivikas666@gmail.com>
2025-09-23 23:43:48 +0530

    half http

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,5 +1,35 @@
 import express from "express";
+import jwt from "jsonwebtoken";
+import { JWT_SECRET } from "./config";
+import { middleware } from "./middleware";
 
 const app = express();
+
+app.post("/singup", (req, res) => {
+  //dbcall
+
+  res.json({
+    userId: "62536",
+  });
+});
+app.post("/signin", (req, res) => {
+  const userId = 1;
+  const token = jwt.sign(
+    {
+      userId,
+    },
+    JWT_SECRET
+  );
+  res.json({
+    token,
+  });
+});
+app.post("/room", middleware, (req, res) => {
+  //dbbcall
+
+  res.json({
+    message: "done",
+  });
+});
 
 app.listen(3001);
