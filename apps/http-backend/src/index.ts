28f9157f5cbaaf9f435629e6e5a11bd03556b45e
vikas jogi <jogivikas666@gmail.com>
2025-09-24 11:19:20 +0530

    prisma compelte

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -2,35 +2,72 @@ import express from "express";
 import jwt from "jsonwebtoken";
 import { JWT_SECRET } from "@repo/backend-common/config";
 import { middleware } from "./middleware";
-import { CreateRoomSchema } from "@repo/common/types";
+import {
+  CreateUserSchema,
+  SigninSchema,
+  CreateRoomSchema,
+} from "@repo/common/types";
+import { prismaClient, PrismaClient } from "@repo/db/client";
 
 const app = express();
+app.use(express.json());
 
-app.post("/singup", (req, res) => {
-  //dbcall
+app.post("/signup", async (req, res) => {
+  const parsedData = CreateUserSchema.safeParse(req.body);
 
-  res.json({
-    userId: "62536",
-  });
+  if (!parsedData.success) {
+    return res.status(400).json({
+      message: "incorrect data",
+    });
+  }
+
+  try {
+    const user = await prismaClient.user.create({
+      data: {
+        email: parsedData.data.username, // make sure schema has "username" or change to "email"
+        password: parsedData.data.password,
+        name: parsedData.data.name,
+      },
+    });
+
+    res.json({
+      userId: user.id, // return real user id instead of hardcoding "1"
+    });
+  } catch (e) {
+    res.status(411).json({
+      message: "user already exists",
+    });
+  }
 });
+
 app.post("/signin", (req, res) => {
+  const data = SigninSchema.safeParse(req.body);
+  if (!data.success) {
+    res.json({
+      message: "incorrect data",
+    });
+    return;
+  }
+
   const userId = 1;
-  const token = jwt.sign(
-    {
-      userId,
-    },
-    JWT_SECRET
-  );
+
+  const token = jwt.sign({ userId }, JWT_SECRET);
+
   res.json({
     token,
   });
 });
-app.post("/room", middleware, (req, res) => {
-  //dbbcall
-
+app.get("/room", middleware, (req, res) => {
+  const data = CreateRoomSchema.safeParse(req.body);
+  if (!data.success) {
+    res.json({
+      message: "incorrect data",
+    });
+    return;
+  }
   res.json({
-    message: "done",
+    roomId: 132,
   });
 });
 
-app.listen(3001);
+app.listen(3000);
